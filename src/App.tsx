699aecc64c625a3f210a9836baaa6e8ad280724e
4twentydev <4twenty.dev@gmail.com>
2025-01-21 06:16:25 -0700

    navdrawer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { ModeToggle } from "./components/ModeToggle";
+import NavDrawer from "./components/NavDrawer";
+
 function App() {
   return (
-    <main className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-red-500 text-3xl">4twentydev</h1>
-    </main>
+    <>
+      <main className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-red-500 text-3xl">4twentydev</h1>
+      </main>
+      <ModeToggle />
+      <NavDrawer />
+    </>
   );
 }
 
