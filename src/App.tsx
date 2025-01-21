c6528809959be289313079b11f3b1eeab68f559e
4twentydev <195083823+4twentydev@users.noreply.github.com>
2025-01-21 05:13:39 -0700

    working copy edit

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ function App() {
   return (
     <main className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-red-500 text-3xl">4twentydev</h1>
+      <h3>Device Unlocks, Key Programming, Emissions</h3>
     </main>
   );
 }
