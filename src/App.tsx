de82974be6f4ab3dcac539a3f86dbd8a5b95bf78
4twentydev <195083823+4twentydev@users.noreply.github.com>
2025-01-21 05:24:38 -0700

    checking auto deployment

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <main className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-red-500 text-3xl">4twentydev</h1>
-      <h3>Device Unlocks, Key Programming, Emissions</h3>
+      <h3 className="text-red-200 text-xl">Device Unlocks, Key Programming, Emissions</h3>
     </main>
   );
 }
