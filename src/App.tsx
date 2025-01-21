94f03c7fde96bf2f98c274f9afe778f47d08294e
4twentydev <4twenty.dev@gmail.com>
2025-01-21 04:51:05 -0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-red-500 text-3xl">4twentydev</h1>
+    </main>
+  );
+}
+
+export default App;
