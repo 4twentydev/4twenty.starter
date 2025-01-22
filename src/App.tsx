509004ecf56c43241bd3758274d8177ad0002f16
4twentydev <4twenty.dev@gmail.com>
2025-01-21 23:49:23 -0700

    some work

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import { ModeToggle } from "./components/ModeToggle";
+import { motion,resct.fl44``1`````````1` } from "motion/react";
 
 function App() {
   return (
     <>
-      <main className="flex flex-col items-center justify-center h-screen bg-var(--background) dark:bg-var(--background)">
-        <h1 className="text-red-500 text-3xl">4twentydev</h1>
+      <motion.div>
         <ModeToggle />
-      </main>
+        
+      </motion.div>
+      <div className="min-h-screen flex flex-col justify-center items-center">
+        <motion.div>
+          <h3 className="text-3xl text-primary">
+            Forget about counseling, rob a bank and keep it bouncing
+          </h3>
+        </motion.div>
+        <motion.div>
+          <p className="text-gray-900 dark:text-secondary  mt-5 mb-10">
+            For details send us a message...
+          </p>
+        </motion.div>
+        <motion.button>Contact</motion.button>
+      </div>
     </>
   );
 }
---
+++ src/components/Contact.tsx
@@ -0,0 +1,13 @@
+const Contact = () => {
+  return (
+    <div className="flex justify-center items-center mx-auto">
+      <script src="https://static.elfsight.com/platform/platform.js" async />
+      <div
+        className="elfsight-app-b234ac8f-899b-46c5-9888-92f3b7662ae4"
+        data-elfsight-app-lazy
+      />
+    </div>
+  );
+};
+
+export default Contact;
