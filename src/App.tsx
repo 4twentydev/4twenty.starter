49e6e91d604484fb7c053aa1ff28a98645366893
4twentydev <4twenty.dev@gmail.com>
2025-01-21 20:30:36 -0700

    darkmode

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
-import { DropdownMenu } from "@radix-ui/react-dropdown-menu";
 import { ModeToggle } from "./components/ModeToggle";
-import NavDrawer from "./components/NavDrawer";
 
 function App() {
   return (
     <>
-      <main className="flex flex-col items-center justify-center h-screen bg-gray-600">
-        <div className="fixed top-0 right-0 border-red-500">
-          <DropdownMenu />
-        </div>
+      <main className="flex flex-col items-center justify-center h-screen bg-var(--background) dark:bg-var(--background)">
         <h1 className="text-red-500 text-3xl">4twentydev</h1>
         <ModeToggle />
-        <div className="flex justify-between items-center">
-          <div className="fixed top-0 left-0 w-full h-10">
-            <NavDrawer />
-          </div>
-        </div>
       </main>
     </>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,14 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   darkMode: ["class"],
-  content: ["./pages/**/*.{html,js}", "./components/**/*.{html,js}"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}", // Note the addition of the `app` directory.
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     container: {
       center: true,
